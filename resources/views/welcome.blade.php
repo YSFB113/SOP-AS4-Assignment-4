8f1e18c3eac152befbdd2527807fd7cdf4d8ca1c
YSFB113 <157676581+YSFB113@users.noreply.github.com>
2024-11-25 15:09:06 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,7 +26,7 @@
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                     <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
                         <div class="flex lg:justify-center lg:col-start-2">
-                            <img src="images/pblogo.png" alt="PB Logo">
+                            <img src="images/666403677375823872.png" alt="PB Logo">
                         </div>
                         @if (Route::has('login'))
                             <nav class="-mx-3 flex flex-1 justify-end">
